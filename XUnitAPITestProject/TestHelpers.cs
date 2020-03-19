2665bb6730c3e5e2cf21dc08c6edf69f6f52a78b
Richad Wysocki <RichardWysocki@gmail.com>
2020-03-19 11:24:37 -0400

    Initial Checkin

---
+++ XUnitAPITestProject/TestHelpers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace XUnitAPITestProject
+{
+    public static class TestHelpers
+    {
+        private static readonly string _projectName = "APIStarterSolution";
+
+        public static Type GetUserType(string fullName)
+        {
+            return (from assembly in AppDomain.CurrentDomain.GetAssemblies()
+                    where assembly.FullName.StartsWith(_projectName)
+                    from type in assembly.GetTypes()
+                    where type.FullName == fullName
+                    select type).FirstOrDefault();
+        }
+    }
+}
